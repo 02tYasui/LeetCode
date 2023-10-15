d3d085e38c1e4d4934d594081190d965e00a2fc1
Taichi Yasui <yasutai12@gmail.com>
2023-10-15 23:22:23 +0900

    commit

--- easy/217_Contains_Duplicate.py
+++ easy/217_Contains_Duplicate.py
@@ -2,10 +2,14 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        size = len(nums)
-        for i in range(size-1):
-            for j in range(i+1, size):
-                if nums[i] == nums [j]:
-                    return True
+        # timeout発生↓
+        # size = len(nums)
+        # for i in range(size-1):
+        #     for j in range(i+1, size):
+        #         if nums[i] == nums [j]:
+        #             return True
 
-        return False
+        # return False
+
+        # set関数で重複している数値を削除し配列に入れなおす→元のnumsとset(nums)が異なれば重複あり
+        return len(nums) != len(set(nums))
